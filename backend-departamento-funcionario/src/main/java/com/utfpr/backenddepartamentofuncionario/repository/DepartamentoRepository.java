23a8cc91a87f7815dcb4e0d54662ae1d3ced1b3a
mariakossar <mariakossar@outlook.com>
2023-10-05 11:31:56 -0300

    backend-departamento-funcionario

--- backend-departamento-funcionario/src/main/java/com/utfpr/backenddepartamentofuncionario/repository/DepartamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utfpr.backenddepartamentofuncionario.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.utfpr.backenddepartamentofuncionario.entity.Departamento;
-
-public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
-
-}
